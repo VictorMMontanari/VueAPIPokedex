f73cc3dbaa7cb7b3ac724d74b1e28890feda1a12
victormmontanari <victormm9000@gmail.com>
2023-08-29 22:33:59 -0300

    Att

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,34 @@
+<script setup>
+const pokemon = defineProps(["name","url"]);
+
+function formatName(inputName) {
+  const parts = inputName.split("-");
+
+  if (parts.length > 1) {
+    const formattedParts = parts.map(
+      (part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
+    );
+    return formattedParts.join(" ");
+  } else {
+    return inputName.charAt(0).toUpperCase() + inputName.slice(1).toLowerCase();
+  }
+}
+</script>
+
+<template>
+  <div class="col-4">
+    <div class="card mb-3">
+      <div class="card" style="width: 18rem">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{{ formatName(pokemon.name) }}</h5>
+          <p class="card-text">
+          </p>
+          <a :href="pokemon.url" class="btn btn-primary">Informações</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,30 @@
-<script setup></script>
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+import ListPokemons from '../components/ListPokemons.vue';
+
+
+let pokemons = reactive(ref());
+
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0")
+  .then(response => response.json())
+  .then(response => {
+    pokemons.value = response.results;
+    console.log(response)
+  })
+})
+
+
+
+</script>
 
 <template>
   <main>
     <div class="container">
       <div class="row mt-4">
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem">
+
+          <!-- <div class="card" style="width: 18rem">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" class="card-img-top" alt="img" />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -13,12 +32,21 @@
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-6">
+          </div> -->
 
+        </div>
+        <div class="col-sm-16 col-md-12">
+            <div class="card">
+              <div class="card-body row">
+                <ListPokemons 
+                  v-for="pokemon in pokemons"
+                  :key="pokemon.name"
+                  :name="pokemon.name"
+                  :url="pokemon.url"
+                 />
+              </div>
+            </div>
         </div>
       </div>
     </div>
