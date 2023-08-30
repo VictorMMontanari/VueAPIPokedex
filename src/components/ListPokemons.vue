fa90144a2c5d3de94a773dee829e6c2658d934ac
Victor M <victormm9000@gmail.com>
2023-08-30 09:18:35 -0300

    Desafio

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -1,25 +1,19 @@
 <script setup>
+import { formatName } from "../functions/formatName";
 const pokemon = defineProps(["name","url"]);
 
-function formatName(inputName) {
-  const parts = inputName.split("-");
+// <-----Desafio Feito----->
+const urlParts = pokemon.url.split('/');
+const idpk = urlParts[urlParts.length - 2];
 
-  if (parts.length > 1) {
-    const formattedParts = parts.map(
-      (part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
-    );
-    return formattedParts.join(" ");
-  } else {
-    return inputName.charAt(0).toUpperCase() + inputName.slice(1).toLowerCase();
-  }
-}
+
 </script>
 
 <template>
   <div class="col-4">
     <div class="card mb-3">
       <div class="card" style="width: 18rem">
-        <img src="..." class="card-img-top" alt="..." />
+        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${idpk}.png`" class="card-img-top" alt="..." />
         <div class="card-body">
           <h5 class="card-title">{{ formatName(pokemon.name) }}</h5>
           <p class="card-text">
