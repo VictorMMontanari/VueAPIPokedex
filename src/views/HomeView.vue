bee7cf851ff9bdb0aa91abee634da9ec1e6eef75
victormmontanari <victormm9000@gmail.com>
2023-09-05 22:27:50 -0300

    Ajustes na Grid Pk

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,12 @@
 import { onMounted, reactive, ref } from 'vue';
 import ListPokemons from '../components/ListPokemons.vue';
 
-
+// Professor ---->
+let caseUrlSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/");
 let pokemons = reactive(ref());
 
 onMounted(() => {
-  fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0")
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=21&offset=0")
   .then(response => response.json())
   .then(response => {
     pokemons.value = response.results;
@@ -37,8 +38,8 @@ onMounted(() => {
 
         </div>
         <div class="col-sm-16 col-md-12">
-            <div class="card">
-              <div class="card-body row">
+            <div class="card text-center">
+              <div class="card-body row g-3">
                 <ListPokemons 
                   v-for="pokemon in pokemons"
                   :key="pokemon.name"
