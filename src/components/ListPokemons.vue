e2a9bd78faca31ae16159ea100445edebfc41ee6
Victor Montanari <victormm9000@gmail.com>
2023-09-06 18:57:42 -0300

    Ajustes

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -10,17 +10,20 @@ const idpk = urlParts[urlParts.length - 2];
 </script>
 
 <template>
-  <div class="col ">
-    <div class="card mb-3" style="width: 18rem; height: 100%;">
-        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${idpk}.png`" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">{{ formatName(pokemon.name) }}</h5>
-          <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${idpk}.gif`" class="" width="39" alt="..." />
-          <a :href="pokemon.url" target="_blank" class="btn btn-primary">Informações</a>
-          <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/${idpk}.gif`" class="" width="39" alt="..." />
-        </div>
+  <div class="col-12 col-sm-4 col-md-3 col-lg-3">
+    <div class="card mb-3" style="height: 100%;">
+      <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${idpk}.png`" class="card-img" alt="..."/>
+      <div class="card-body text-center">
+        <h5 class="card-title">{{ formatName(pokemon.name) }}</h5>
+        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${idpk}.gif`" class="" width="39" alt="..." />
+        <a :href="pokemon.url" target="_blank" class="btn btn-primary">Informações</a>
+        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/${idpk}.gif`" class="" width="39" alt="..." />
+      </div>
     </div>
   </div>
 </template>
+
+
+
 
 <style></style>
