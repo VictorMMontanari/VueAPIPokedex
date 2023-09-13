e5a3ef818e30a7f6d6fba16e5205de1595e0790c
Victor Montanari <victormm9000@gmail.com>
2023-09-13 18:15:15 -0300

    Atualização Tipo Pokemon

--- src/functions/formatName.ts
+++ src/functions/formatName.ts
@@ -13,4 +13,26 @@ function formatName(inputName) {
   }
 }
 
-export { formatName };
+const getTypeStyle = (typeName) => {
+  const typeStyles = {
+      bug: 'bug', flying: 'flying', dark: 'dark', dragon: 'dragon',
+      electric: 'electric', fighting: 'fighting', fire: 'fire', ghost: 'ghost',
+      grass: 'grass', ground: 'ground', ice: 'ice', normal: 'normal',
+      poison: 'poison', psychic: 'psychic', rock: 'rock', steel: 'steel',
+      water: 'water', fairy: 'fairy'
+  };
+
+  return typeStyles[typeName.toLowerCase()] || '';
+};
+
+const formatType = (typeName) => {
+  const typeNames = {
+      normal: 'Normal', fire: 'Fire', water: 'Water', grass: 'Grass',
+      flying: 'Flying', fighting: 'Fighting', poison: 'Poison', electric: 'Electric',
+      ground: 'Ground', rock: 'Rock', psychic: 'Psychic', ice: 'Ice',
+      bug: 'Bug', ghost: 'Ghost', steel: 'Steel', dragon: 'Dragon', dark: 'Dark', fairy: 'Fairy'
+  };
+  return typeNames[typeName.toLowerCase()] || typeName;
+};
+
+export { formatName, formatType, getTypeStyle};
