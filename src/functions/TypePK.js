356441874f936dbe3be0b0397133a1084f76e707
Victor M <victormm9000@gmail.com>
2023-09-26 10:59:04 -0300

    add dist

--- src/functions/TypePK.js
+++
@@ -1,7 +0,0 @@
-import { ref } from 'vue';
-
-export const selectedOption = ref('ALL...'); 
-export function handleSelectedOption(option) {
-  selectedOption.value = option;
-  return selectedOption.value; 
-}
